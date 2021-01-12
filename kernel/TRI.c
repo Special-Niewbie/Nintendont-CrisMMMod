a26b23cf9f370779ee6237db4ac1d96582ff295b
AnthonyRyuki <62845774+AnthonyRyuki@users.noreply.github.com>
2021-01-12 13:47:36 +0100

    Update TRI.c

--- kernel/TRI.c
+++ kernel/TRI.c
@@ -354,7 +354,7 @@ void TRISetupGames()
 	{
 		dbgprintf("TRI:F-Zero AX (Rev C)\r\n");
 		TRIGame = TRI_AX;
-		SystemRegion = REGION_JAPAN;
+		SystemRegion = REGION_EXPORT; //Setting the region to JAPAN will change the game's language to Japanese.
 		AXTimerOffset = 0x003CD1C0;
 		TRISettingsName = SETTINGS_AX_RVC;
 		TRISettingsLoc = 0x3CF6F0;
@@ -386,9 +386,6 @@ void TRISetupGames()
 
 		//Remove Overscan on first VIConfigure
 		write32( 0x001FB998, 0x38C00000 );
-
-		//English
-		write32( 0x000DF430, 0x38000000 );
 
 		if(!arcadeMode)
 		{
@@ -437,7 +434,7 @@ void TRISetupGames()
 	{
 		dbgprintf("TRI:F-Zero AX (Rev D)\r\n");
 		TRIGame = TRI_AX;
-		SystemRegion = REGION_JAPAN;
+		SystemRegion = REGION_EXPORT; //Setting the region to JAPAN will change the game's language to Japanese.
 		AXTimerOffset = 0x003CD6A0;
 		TRISettingsName = SETTINGS_AX_RVD;
 		TRISettingsLoc = 0x3CFBD0;
@@ -479,9 +476,6 @@ void TRISetupGames()
 
 		//Remove Overscan on first VIConfigure
 		write32( 0x001FBE54, 0x38C00000 );
-
-		//English
-		write32( 0x000DF698, 0x38000000 );
 
 		if(!arcadeMode)
 		{
@@ -531,7 +525,7 @@ void TRISetupGames()
 	{
 		dbgprintf("TRI:F-Zero AX (Rev E)\r\n");
 		TRIGame = TRI_AX;
-		SystemRegion = REGION_JAPAN;
+		SystemRegion = REGION_EXPORT; //Setting the region to JAPAN will change the game's language to Japanese.
 		AXTimerOffset = 0x003CDC20;
 		TRISettingsName = SETTINGS_AX_RVE;
 		TRISettingsLoc = 0x3D0150;
@@ -570,9 +564,6 @@ void TRISetupGames()
 
 		//Remove Overscan on first VIConfigure
 		write32( 0x001FC2C4, 0x38C00000 );
-
-		//English
-		write32( 0x000DF818, 0x38000000 );
 
 		if(!arcadeMode)
 		{
