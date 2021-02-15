583e7c6389b793f3dce43328cc3a8f3e6be0c1ce
AnthonyRyuki <62845774+AnthonyRyuki@users.noreply.github.com>
2021-02-15 21:52:05 +0100

    Interrompere la musica di sottofondo quando inizia l'animazione e non mostrarla se è un gioco Triforce
    C'è però un problema che non sono riuscito a risolvere: quando cerco di far partire il gioco con il vero file di IPL, il loader non passa allo sfondo di caricamento del gioco e va in crash. La cosa strana è che succede con i giochi GC, ma non con quelli del Triforce. Avresti idea di come risolvere?

--- loader/source/main.c
+++ loader/source/main.c
@@ -1472,8 +1472,11 @@ int main(int argc, char **argv)
 	}
 //	memcpy( (void*)0x80000000, (void*)0x90140000, 0x1200000 );
 	
-	if (!useipl && (!(ncfg->Config & (NIN_CFG_SKIP_IPL))))
+	if ((!useipl || !useipltri) && (!(ncfg->Config & (NIN_CFG_SKIP_IPL))))
+	{
+		MP3Player_Stop();
 		Animation();
+	}
 	
 	//GRRLIB_FreeTexture(background);
 	GRRLIB_FreeTexture(screen_buffer);
